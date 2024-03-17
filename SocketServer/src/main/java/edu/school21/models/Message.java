c7da623b1fc5f6f8114b0a5a64ea923a073503aa
Kaleigh Hyde <kaleighh@si-a8.nsk.21-school.ru>
2024-03-17 22:58:31 +0700

    start json

--- SocketServer/src/main/java/edu/school21/models/Message.java
+++ SocketServer/src/main/java/edu/school21/models/Message.java
@@ -1,5 +1,6 @@
 package edu.school21.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,27 @@ import java.time.format.DateTimeFormatter;
 @Data
 public class Message {
     private User sender;
+    @JsonProperty("text")
     private String text;
     private LocalDateTime time;
     private Chatroom room;
+
+    @JsonProperty("sender_login")
+    private String getSenderLogin() {
+        return sender.getLogin();
+    }
+
+    @JsonProperty("time")
+    private String getTime() {
+        return time.format(DateTimeFormatter.ofPattern("dd.MM HH:mm"));
+    }
+
+    public String toJsonString() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+
+        return mapper.writeValueAsString(this);
+    }
 
     @Override
     public String toString() {
