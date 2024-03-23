3a1ea1a055b8bd63aa97363b9a364d117c85bd21
kaleighh <kaleighh@s21.ru>
2024-03-24 01:34:08 +0800

    no more 3000 s

--- SocketServer/src/main/java/edu/school21/app/Main.java
+++ SocketServer/src/main/java/edu/school21/app/Main.java
@@ -7,16 +7,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 3002; i++) {
-            System.out.print("s");
-        }
         ApplicationContext context = new AnnotationConfigApplicationContext(SocketsApplicationConfig.class);
         Server server = context.getBean(Server.class);
         server.setPort(getPort(args));
         server.run();
     }
 
-    private static int getPort(String[] args) {
+    public static int getPort(String[] args) {
         if (args.length != 1 || !args[0].startsWith("--port=")) {
             System.err.println("Please write port in arguments.");
             System.exit(-1);
